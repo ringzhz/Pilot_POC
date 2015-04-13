2a3083f8f9f68a9f0e1dd4432d42f34a33ff5dbd
Mike Partain <spiked3@gmail.com>
2015-04-13 05:23:05 -0700

    another reorg now that it is compiling. Arduino does things differently bug heading reset to non 0 heading still broke

--- ArduinoPilot/ArduinoPilot.ino
+++ ArduinoPilot/ArduinoPilot.ino
@@ -1,17 +1,16 @@
 //* S3 Pilot Proof of Concept, Arduino UNO shield
-//* Copyright � 2015 Mike Partain, MWPRobotics dba Spiked3.com, all rights reserved
+//* Copyright (c) 2015 Mike Partain, MWPRobotics dba Spiked3.com, all rights reserved
+
+#pragma once
 
 #include <Wire.h>
 #include <ArduinoJson.h>
 
 #include "I2Cdev.h"
 #include "MPU6050_6Axis_MotionApps20.h"
-
-#include <Wire.h>
-
 #include "digitalWriteFast.h"
 
-#ifdef DEBUG
+#if 1 
 #define DBGLOG(x) Serial.print(x)
 #else
 #define DBGLOG(x)
@@ -32,9 +31,7 @@
 #define M1_FB  16	// A2
 #define M2_FB  15	// A1
 
-#define toggle(X) digitalWrite(X,!digitalRead(X))
-
-#define Sign(A) (A >= 0 ? 1 : -1)
+const char *Topic = "Topic";
 
 ////////////////////////////////////////////////////////////
 
@@ -45,56 +42,13 @@ typedef struct {
 	float EncoderScaler;	// calculated
 } Geometry;
 
-struct CmdFunction
-{
-	const char *cmd;
-	bool(*f)(JsonObject&  j);
-};
-
-float X = 0.0;		// internally mm, broadcast in meters
+float X = 0.0;		// internally mm, published in meters
 float Y = 0.0;
-float H = 0.0;		// internally using radians, broadcasts in degrees
+float H = 0.0;		// internally using radians, published in degrees
 
 uint64_t LastPoseTime = 0L;
-float previousHeading = 0.0;
-
-class PilotMotor
-{
-public:
-	char motorName[3];
-	char safetyEOS = '\0';
-	uint8_t pwmPin;
-	uint8_t dirPin;
-	uint8_t feedBackPin;
-	uint8_t interruptIndex;
-	bool reversed;
-
-	uint32_t lastUpdateTime;
-	uint32_t lastTacho;
-	float desiredSpeed;
-	float actualSpeed;
-
-	float lastPower;
-	float previousError;
-	float previousIntegral;
-
-	PilotMotor(const char *name, uint8_t pwm, uint8_t dir, uint8_t fb, uint8_t idx, bool revrsd);
-	void Reset();
-	uint32_t GetTacho();
-	void SetSpeed(int spd);
-
-	void Tick();
-};
-
-uint8_t MotorMax = 100;
-float Kp1, Ki1, Kd1;		// per motor regulator
-float Kp2, Ki2, Kd2;		// synchronizing regulator
-
-volatile uint32_t tacho[2];		// interrupt 0 & 1 tachometers
-
-/// Globals ///////////////////////////////////////////////
-
-const char *Topic = "Topic";
+float previousYaw = 0.0;
+uint8_t lastBumperRead = 0xff;
 
 bool AhrsEnabled = true;
 bool escEnabled = true;
@@ -104,8 +58,12 @@ bool DestinationEventEnabled = true;
 bool pingEventEnabled = false;
 bool PoseEventEnabled = true;
 
-Geometry Geom;
-MPU6050 mpu;
+// counter based (ie every X cycles)
+uint16_t CalcPoseFrequency = 100;		// +++ aim for 20-30 / sec
+uint16_t regulatorFrequency = 100;
+uint16_t motorRegulatorFrequency = 50;
+uint16_t heartbeatEventFrequency = 5000;
+uint64_t cntr = 0L;
 
 // MPU control/status vars
 bool dmpReady = false;  // set true if DMP init was successful
@@ -120,49 +78,26 @@ VectorFloat gravity;    // [x, y, z]            gravity vector
 float euler[3];         // [psi, theta, phi]    Euler angle container
 float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
+#include "motor.h"
+#include "commands.h"
+#include "publish.h"
+
+Geometry Geom;
+MPU6050 mpu;
+
+PilotMotor	M1("M1", M1_PWM, M1_DIR, M1_FB, 0, false),
+			M2("M2", M2_PWM, M2_DIR, M2_FB, 1, true);
+
+int  mqIdx = 0;
+char mqRecvBuf[128];
+bool dmp_rdy = false;
+
 // FYI
 //#define clockCyclesPerMicrosecond() ( F_CPU / 1000000L )
 //#define clockCyclesToMicroseconds(a) ( (a) / clockCyclesPerMicrosecond() )
 //#define microsecondsToClockCycles(a) ( (a) * clockCyclesPerMicrosecond() )
 
-// counter based (ie every X cycles)
-uint16_t CalcPoseFrequency = 100;		// +++ aim for 20-30 / sec
-uint16_t regulatorFrequency = 100;
-uint16_t motorRegulatorFrequency = 50;
-uint16_t heartbeatEventFrequency = 5000;
-uint64_t cntr = 0L;
-
-// +++ change so uses escEnabled instead of -1
-// if pins are set to -1, they will not be used, index is the tacho interrupt array
-PilotMotor	M1("M1", M1_PWM, M1_DIR, M1_FB, 0, false),
-M2("M2", M2_PWM, M2_DIR, M2_FB, 1, true);
-
-int  mqIdx = 0;
-char mqRecvBuf[128];
-
-bool dmp_rdy = false;
-
-/////////////////////////////////////////////////////
-
-ISR(MotorISR1)
-{
-	int b = digitalReadFast(8);
-	if (digitalReadFast(2))
-		b ? tacho[0]++ : tacho[0]--;
-	else
-		b ? tacho[0]-- : tacho[0]++;
-}
-
-ISR(MotorISR2)
-{
-	int b = digitalReadFast(9);
-	if (digitalReadFast(3))
-		b ? tacho[1]++ : tacho[1]--;
-	else
-		b ? tacho[1]-- : tacho[1]++;
-}
-
-/////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
 
 void Log(String t)
 {
@@ -175,43 +110,8 @@ void Log(String t)
 	Serial.print('\n');
 }
 
-//////////////////////////////////////////////////
-
-void MotorInit()
-{
-	Serial.print("// MotorInit ... \n");
-
-	tacho[0] = tacho[1] = 0L;
-
-	// !! hardcoded interrupt handlers !!
-	pinMode(2, INPUT_PULLUP);
-	pinMode(3, INPUT_PULLUP);
-	pinMode(8, INPUT_PULLUP);
-	pinMode(9, INPUT_PULLUP);
-
-	attachInterrupt(PCINT0, MotorISR1, CHANGE);	// pin 2
-	attachInterrupt(PCINT1, MotorISR2, CHANGE); // pin 3
-
-	if (escEnabled)
-	{
-		pinMode(M1_PWM, OUTPUT);
-		digitalWrite(M1_PWM, 0);
-		pinMode(M2_PWM, OUTPUT);
-		digitalWrite(M2_PWM, 0);
-		pinMode(M1_DIR, OUTPUT);
-		digitalWrite(M1_DIR, 0);
-		pinMode(M2_DIR, OUTPUT);
-		digitalWrite(M2_DIR, 0);
-		M1.Reset();
-		M2.Reset();
-	}
-}
-
-void PilotRegulatorTick()
-{
-	// +++ regulation needed now! but waiting on hardware :|
-}
-
+// escEnabled serves 2 purposes. if it is false at startup, the pins are not initialized
+// after startup it is used to actually enable/disable the speed controlers
 
 void setup()
 {
@@ -284,56 +184,6 @@ void setup()
 	Serial.print(F("SUB:Cmd/robot1\n"));		// subscribe only to messages targetted to us
 }
 
-void PublishPose()
-{
-	StaticJsonBuffer<128> jsonBuffer;
-	JsonObject& root = jsonBuffer.createObject();
-	root[Topic] = "robot1";
-	root["T"] = "Pose";
-	root["X"].set(X / 1000, 4);		// mm to meter
-	root["Y"].set(Y / 1000, 4);
-	root["H"].set(RAD_TO_DEG * H, 2);
-	root.printTo(Serial); Serial.print('\n');
-}
-
-void PublishHeartbeat()
-{
-	StaticJsonBuffer<128> jsonBuffer;
-	JsonObject& root = jsonBuffer.createObject();
-	root[Topic] = "robot1";
-	root["T"] = "Heartbeat";
-
-#if 1
-	root["M1Tach"].set(M1.GetTacho(), 0);  // 0 is the number of decimals to print
-	root["M2Tach"].set(M2.GetTacho(), 0);
-	if (AhrsEnabled)
-	{
-		root["Yaw"].set(ypr[0], 4);
-		root["Pit"].set(ypr[1], 4);
-		root["Rol"].set(ypr[2], 4);
-	}
-#endif
-
-	root.printTo(Serial); Serial.print('\n');
-}
-
-//////////////////////////////////////////////////
-
-void MqLine(char *line, int l)
-{
-	char t[64];
-	const char *T;
-	StaticJsonBuffer<256> jsonBuffer;
-	JsonObject& j = jsonBuffer.parseObject(line);
-
-	if (strcmp((const char *)j["T"], "Cmd") == 0)
-		ProcessCommand(j);
-	else
-	{
-		sprintf_P(t, "// rcv <- missing or unrecognized T \"%s\"\n", j["T"]); Serial.print(t);
-	}
-}
-
 void CheckMq()
 {
 	if (Serial.available())
@@ -344,9 +194,19 @@ void CheckMq()
 		if (c == '\n')		// end of line, process
 		{
 			mqRecvBuf[mqIdx++] = '\0';
-			MqLine(mqRecvBuf, mqIdx);
+			StaticJsonBuffer<256> jsonBuffer;
+			JsonObject& j = jsonBuffer.parseObject(mqRecvBuf);
+			if (strcmp((const char *)j["T"], "Cmd") == 0)
+				ProcessCommand(j);
+			else
+			{
+				char t[64];
+				sprintf(t, "// rcv <- missing or unrecognized T \"%s\"\n", j["T"]);
+				Serial.print(t);
+			}
 			memset(mqRecvBuf, 0, mqIdx);
 			mqIdx = 0;
+			return;
 		}
 		else
 			mqRecvBuf[mqIdx++] = c;
@@ -360,10 +220,6 @@ void CheckMq()
 	}
 }
 
-void BumperEvent()
-{
-}
-
 bool CalcPose()
 {
 	bool poseChanged = false;
@@ -375,7 +231,7 @@ bool CalcPose()
 		delta2 = tachoNow2 - M2.lastTacho;
 
 	// uses DMP for heading
-	float headingDelta = (ypr[0] - previousHeading);
+	float headingDelta = (ypr[0] - previousYaw);
 
 	if (abs(RAD_TO_DEG * headingDelta) > .1F)
 		poseChanged = true;
@@ -394,235 +250,13 @@ bool CalcPose()
 	if (H >= TWO_PI)
 		H -= TWO_PI;
 
-	previousHeading = ypr[0];
+	previousYaw = ypr[0];
 
 	M1.lastTacho = tachoNow1;
 	M2.lastTacho = tachoNow2;
 
 	return poseChanged;
 }
-
-//////////////////////////////////////////////////
-uint8_t lastBumperRead = 0xff;
-
-bool cmdTest1(JsonObject&  j)
-{
-	DBGLOG(F("::cmdTest1"));
-	Serial.print(F("// cmdTest1\n"));
-	return true;
-}
-
-//////////////////////////////////////////////////
-
-bool cmdMmax(JsonObject&  j)
-{
-	DBGLOG(F("::cmdMmax"));
-	MotorMax = j["Value"];
-	return true;
-}
-
-bool cmdPid1(JsonObject&  j)
-{
-	DBGLOG(F("::cmdPid1"));
-	Kp1 = j["P"];
-	Ki1 = j["I"];
-	Kd1 = j["D"];
-	return true;
-}
-
-bool cmdBump(JsonObject&  j)
-{
-	DBGLOG(F("::cmdBump"));
-	BumperEventEnabled = j["Value"] == 1;
-	return true;
-}
-
-bool cmdDest(JsonObject&  j)
-{
-	DBGLOG(F("::cmdDest"));
-	DestinationEventEnabled = j["Value"] == 1;
-	return true;
-}
-
-bool cmdHeartbeat(JsonObject&  j)
-{
-	DBGLOG(F("::cmdHeartbeat"));
-	heartbeatEventEnabled = j["Value"] == 1;
-	if (j.containsKey("Int"))
-		heartbeatEventFrequency = j["Int"];
-	return true;
-}
-
-bool cmdPing(JsonObject&  j)
-{
-	DBGLOG(F("::cmdPing"));
-	pingEventEnabled = j["Value"] == 1;
-	return true;
-}
-
-bool cmdReset(JsonObject&  j)
-{
-	// by including specific variables, you can set pose to a particular value
-	DBGLOG(F("::cmdReset"));
-	M1.Reset();
-	M2.Reset();
-	X = Y = H = previousHeading = 0.0;
-	if (j.containsKey("X"))
-		X = j["X"];
-	if (j.containsKey("Y"))
-		Y = j["Y"];
-	if (j.containsKey("H"))
-		H = DEG_TO_RAD * (float)j["H"];
-
-	previousHeading = ypr[0];	// base value
-
-	return true;
-}
-
-bool cmdEsc(JsonObject&  j)
-{
-	DBGLOG(F("::cmdEsc"));
-	escEnabled = j["Value"] == 1;
-	digitalWriteFast(ESC_ENA, escEnabled);
-	return true;
-}
-
-bool cmdPose(JsonObject&  j)
-{
-	DBGLOG(F("::cmdPose"));
-	PoseEventEnabled = j["Value"] == 1;
-	if (j.containsKey("Int"))
-		CalcPoseFrequency = j["Int"];
-	return true;
-}
-
-bool cmdGeom(JsonObject&  j)
-{
-	// +++
-	DBGLOG(F("::cmdGeom"));
-	return false;
-}
-
-bool cmdPower(JsonObject&  j)
-{
-	// +++ actually more of a testing function, will probably go away
-	//char t[16];
-	DBGLOG(F("::cmdPower"));
-
-	if (escEnabled)
-	{
-		int p = constrain((int)j["Value"], -100, 100);
-		//sprintf(t, "// P %d\n", p); Serial.print(t);
-		M1.SetSpeed(p);
-		M2.SetSpeed(p);
-	}
-
-	return true;
-}
-
-bool cmdMove(JsonObject&  j)
-{
-	DBGLOG(F("::cmdMove"));
-	float speed = (int)j["Speed"] / 10.0F;
-
-	return false;
-}
-
-bool cmdRot(JsonObject&  j)
-{
-	DBGLOG(F("::cmdRot"));
-	float speed = (int)j["Speed"] / 10.0F;
-
-	return false;
-}
-
-bool cmdGoto(JsonObject&  j)
-{
-	DBGLOG(F("::cmdGoto"));
-	float speed = (int)j["Speed"] / 10.0F;
-
-	return false;
-}
-
-////////////////////////////////////////////////////
-
-CmdFunction cmdTable[] {
-	{ "Test1", cmdTest1 },
-	{ "Reset", cmdReset },
-	{ "Geom", cmdGeom },
-	{ "MMax", cmdMmax },
-	{ "PID1", cmdPid1 },
-	{ "Esc", cmdEsc },
-	{ "Rot", cmdRot, },
-	{ "GoTo", cmdGoto, },
-	{ "Move", cmdMove },
-	{ "Bump", cmdBump, },
-	{ "Dest", cmdDest, },
-	{ "Heartbeat", cmdHeartbeat, },
-	{ "Pose", cmdPose, },
-	{ "Power", cmdPower, },
-	{ "Ping", cmdPing, },
-};
-
-void ProcessCommand(JsonObject& j)
-{
-	for (int i = 0; i < sizeof(cmdTable) / sizeof(cmdTable[0]); i++)
-		if (strcmp(cmdTable[i].cmd, (const char *)j["Cmd"]) == 0)
-		{
-			bool rc = (*cmdTable[i].f)(j);
-			break;
-		}
-}
-
-PilotMotor::PilotMotor(const char *name, uint8_t pwm, uint8_t dir, uint8_t fb, uint8_t idx, bool revrsd)
-{
-	strncpy(motorName, name, sizeof(motorName));
-	pwmPin = pwm;
-	dirPin = dir;
-	feedBackPin = fb;
-	interruptIndex = idx;
-	reversed = revrsd;
-
-	if (escEnabled)
-	{
-		pinMode(pwm, OUTPUT);
-		pinMode(dir, OUTPUT);
-	}
-
-	Reset();
-}
-
-void PilotMotor::Reset()
-{
-	tacho[interruptIndex] = lastTacho = 0L;
-	lastPower = desiredSpeed = actualSpeed = 0.0;
-	previousError = previousIntegral = 0.0;
-	//lastUpdateTime = ? ? ? ;
-}
-
-uint32_t PilotMotor::GetTacho()
-{
-	return reversed ? -tacho[interruptIndex] : tacho[interruptIndex];
-}
-
-void PilotMotor::SetSpeed(int spd)
-{
-	char t[64];
-	// speed is a +/- percent of max	
-	uint8_t newDir = (spd >= 0) ? (reversed ? 1 : 0) : (reversed ? 0 : 1);
-	int16_t newSpeed = map(abs(spd), 0, 100, 0, 255 * MotorMax / 100);
-	digitalWrite(dirPin, newDir);
-	analogWrite(pwmPin, newSpeed);
-	desiredSpeed = newSpeed;
-	sprintf(t, "// %d,%d >> %s\n", newDir, newSpeed, motorName); Serial.print(t);
-}
-
-void PilotMotor::Tick()
-{
-
-}
-
-///////////////////////////////////////////////////
 
 void loop()
 {
@@ -649,7 +283,7 @@ void loop()
 	if (escEnabled && (cntr % regulatorFrequency == 0))			// PID regulator
 		PilotRegulatorTick();
 
-	// +++ calc pose really needs to be done dmp interrupt or x ms for encoders
+	// +++ calc pose really needs to be done every dmp interrupt & x ms for encoders
 	// or at least integrated
 	//  since likelyhood of driving perfectly straight are slim
 	//  just using dmp interrupts will probably be ok
---
+++ ArduinoPilot/publish.h
@@ -0,0 +1,39 @@
+//* S3 Pilot Proof of Concept, Arduino UNO shield
+//* Copyright (c) 2015 Mike Partain, MWPRobotics dba Spiked3.com, all rights reserved
+
+void PublishPose()
+{
+	StaticJsonBuffer<128> jsonBuffer;
+	JsonObject& root = jsonBuffer.createObject();
+	root[Topic] = "robot1";
+	root["T"] = "Pose";
+	root["X"].set(X / 1000, 4);		// mm to meter
+	root["Y"].set(Y / 1000, 4);
+	root["H"].set(RAD_TO_DEG * H, 2);
+	root.printTo(Serial); Serial.print('\n');
+}
+
+void PublishHeartbeat()
+{
+	StaticJsonBuffer<128> jsonBuffer;
+	JsonObject& root = jsonBuffer.createObject();
+	root[Topic] = "robot1";
+	root["T"] = "Heartbeat";
+
+#if 1
+	root["M1Tach"].set(M1.GetTacho(), 0);  // 0 is the number of decimals to print
+	root["M2Tach"].set(M2.GetTacho(), 0);
+	if (AhrsEnabled)
+	{
+		root["Yaw"].set(ypr[0], 4);
+		root["Pit"].set(ypr[1], 4);
+		root["Rol"].set(ypr[2], 4);
+	}
+#endif
+
+	root.printTo(Serial); Serial.print('\n');
+}
+
+void BumperEvent()
+{
+}
